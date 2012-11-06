a129f89b21a56a72dab2ca60ab653f33ff8c8b6b
Arijit Choudhury <arijit.x.choudhury@accenture.com>
2012-11-06 18:40:20 +0530

    project

---
+++ src/arijit/github/HelloWorld.java
@@ -0,0 +1,15 @@
+package arijit.github;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git Eclipse Project!!!");
+
+	}
+
+}
